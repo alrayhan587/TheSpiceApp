86b88cfed94c96a00cb73dc48cf13e3e6ee7412f
alrayhan587 <alrayhan587@gmail.com>
2024-03-31 16:50:48 +0600

    updatedcode

--- Screens/ConversationScreen.js
+++ Screens/ConversationScreen.js
@@ -1,48 +1,115 @@
 /* eslint-disable no-shadow */
 import { View, Text, Image } from 'react-native'
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState, useCallback, useEffect, Component } from 'react'
 import { GiftedChat } from 'react-native-gifted-chat'
+import { Dialogflow_V2 } from "react-native-dialogflow"
+import { dialogflowConfig } from '../env'
+
+const botAvatar = require('./Assets/bot-icon.webp')
 
 
-
-
-const ConversationScreen = () => {
-  const botAvatar = require('./Assets/bot-icon.webp')
-  const [messages, setMessages] = useState([])
-
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: botAvatar,
-        },
-      },
-    ])
-  }, [])
-
-  const onSend = useCallback((messages = []) => {
-    setMessages(previousMessages =>
-      GiftedChat.append(previousMessages, messages),
-    )
-  }, [])
-
-  const onQuickReply=()=>{}
-
-  return (
-    <GiftedChat
-      messages={messages}
-      onSend={messages => onSend(messages)}
-      onQuickReply={(quickReply) => this.onQuickReply(quickReply)}
-      user={{
-        _id: 1,
-      }}
-    />
-  )
+const Bot = {
+  _id: 2,
+  name: 'Bot',
+  avatar: botAvatar,
 }
 
-export default ConversationScreen
+
+class ConversationScreen extends Component {
+
+
+  state = {
+    messages: [
+      {
+        _id: 2,
+        text: "How may i help you?",
+        createdAt: new Date(),
+        user: Bot
+      },
+      {
+        _id: 1,
+        text: "Hello Developer!!",
+        createdAt: new Date(),
+        user: Bot
+      }
+    ],
+    id: 1,
+    name: '',
+  };
+
+  componentDidMount = () => {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id,
+    );
+  }
+
+  handleGoogleResponse(result) {
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+
+    this.sendBotResponse(text);
+  }
+
+  sendBotResponse(text) {
+    let msg = {
+      _id: this.state.messages.length + 1,
+      text,
+      createdAt: new Date(),
+      user: Bot
+    }
+
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, [msg]),
+    }));
+  }
+
+
+  onSend = (messages = []) => {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }))
+    let message = messages[0].text;
+
+
+    Dialogflow_V2.requestQuery(
+      message,
+      (result) => this.handleGoogleResponse(result),
+      (error) => console.log(error)
+    )
+  }
+
+
+
+  onQuickReply = (quickReply) => {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, quickReply)
+    }))
+    let messages = quickReply[0].value;
+
+    Dialogflow_V2.requestQuery(
+      messages,
+      (result) => this.handleGoogleResponse(result),
+      (error) => console.log(error)
+    )
+  }
+
+  render() {
+    return (
+      <View className='flex-1 bg-white' >
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          onQuickReply={(quickReply) => this.onQuickReply(quickReply)}
+          user={{
+            _id: 1,
+          }}
+        />
+      </View>
+
+    )
+  }
+}
+
+export default ConversationScreen;
---
+++ env.js
@@ -0,0 +1,13 @@
+export const dialogflowConfig = {
+    "type": "service_account",
+    "project_id": "spicebee-iyyw",
+    "private_key_id": "fe64ba77dfc9026edeeee5c6023a1b7ab3582a88",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCyJeGNi8ftucq6\nXpcTiSiGoJ5WOXyI8uAast2uhCy9rUTzJLKU1Ha9y3fN1gkIer8N0acr3ubPrDy2\nZxgY6f7pNoTjaIje63sDNhZVCprjNcJJvfV95TTPbywbjgleXTqtkkR2uwrey4XD\n0Xlm4uUfqlsUTO8gx5YOCK1q865rzpU2BFuuqbTPCxRes8wIXEPjPXf4SJHfqlWH\nVrYVD9R64sv/2AL+mq9wXV9VCOiafXvJplxcrcz9YYGkd5YLiAPG7puXzCVturhe\n6eGWNttvqkjwE2cLCYWnCsvND4mTOhqDEo9cxwft18kRFPwdcj+HkulN5iXnoIZY\nUcoHAQLfAgMBAAECggEARk9IFW7RIeR9fA/ADZBJA9VZmItIuN4uj6r/bhSxeczs\nVRArQS0XjjHDH2R1nTigteoEUf6/1r/SGGVY7+BOYKnJSd1kg6KpoqtSpg6w8bKy\ncxs7TAxNyv7ECD4HOYyPD5kWV0vb7vGhYhcHFJHBPz2jg8AXcrcC5JgNJZR2RwqR\nS23Q5RKNrabSlqjoxnQcPXeAwLKghUMV/XLNWiNUcS8qJMHJs8GuJRRpIpp1OSNG\nvhMumF9cYekdBaMgTHx1ed+Cn/SgP6MmYAOsSstV+qXT2VrwipO2bgMH+TS2OGWR\n+5sgRNtW6erA/POa8a7MruGZlE5ii9vLydZEr5B11QKBgQDeDHvQLUSJULu12qnO\nc466brJuC2LkPZdF2ZJO6hVaXvSmpmWVTlzwlvxIKBE9xl9n0N6Pnx64k3kSvCWg\nLhY8XV0tpmRteMSNpssiBxDrXyFbNPLOBgUZYP8liQWx0VBIFsl/Rbqjl5L+d01D\na36FsZtm2IS7mocXBvtLkKulTQKBgQDNYwQgAurcYb8LKlbeb4aPo+yemzw9BGTK\nhUtFcXh0ZKt4GNUxzxt1BGrXnV2mvrbnfBMWVi51kYHeHJdwwmR2YmhQ3Wo7AVCC\ngdKf7C/fOrbgi77vxlHqCX0q1WJbBmZyGGJrzdTnmmFx98Y/b1IWr6BrESpMX8Wz\npxaFsGAC2wKBgCPsqx9sI3l/2U67uNtQ8NeSz0VkjDRXDqFbaQNhdeDwGe7YXvj/\n+6NlGh3mdZuj+42FSmy8FrFQEreIPyIiMrDDfi7Di932Ov6b7KNttzwNYQ0rjNju\nOmb3vW/qMkSpk66GGUWBF4geHscSsCpnOsoQiQ7jmt3rS03bcE6yckLFAoGAOYNw\ntEYc3nnAta+1g7RXSgHtULzHsY55avzIhzmvMWi/W3IYwUnttmb2REqc3wSo00m7\nTpXzjJiidUAsaDcHSt9jtsFJ7hwAH4ubH41UGn6ml1qKrTor1g2yLzxxvmucZHbP\nmMCazgA9KVzo9vqFUjT8Bv9XiOuIwTYY6Hbra+ECgYBcpHVbHca1MlI9Km3T4aIW\nSfWPXXlzAcAUS4QO7ag7JgfYWB0wbzKZ3RWRzO69PE/8enXgyE05I7cCyWDX586p\nKuF3lf/Rxm20TGgazHywwgmIZI9WHU+B5R6BjaxSVk6LbfwJpUJX44vMReY5xRIg\nUXV56aAbSIpPiTx45Pj3kQ==\n-----END PRIVATE KEY-----\n",
+    "client_email": "spicebee-lesson@spicebee-iyyw.iam.gserviceaccount.com",
+    "client_id": "117257207523001562989",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/spicebee-lesson%40spicebee-iyyw.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
